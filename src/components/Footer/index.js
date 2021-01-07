7df15237555c14ca8aff9f0f7f5015b910e11c48
Dina Saunders <dina_saunders@mac.com>
2021-01-07 11:50:06 -0500

    added Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+    return (
+        <footer class="footer mt-auto py-3">
+            <div class="container">
+                <span class="text-muted">DSaunders Portfolio 2021</span>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
