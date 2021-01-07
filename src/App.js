9fdf72cd58170d4e106e4a2e14c3158691a253f7
Dina Saunders <dina_saunders@mac.com>
2021-01-07 11:55:32 -0500

    added Wrapper component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Wrapper>
+
+      </Wrapper>
       <Footer />
     </div>
   );
