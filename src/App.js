2aa52f3afad7894f9a69cddedc8cdd3598ecf3ff
Dina Saunders <dina_saunders@mac.com>
2021-01-10 13:40:16 -0500

    added react router and home and page routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper"
@@ -6,13 +9,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Wrapper>
-
-      </Wrapper>
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/about" component={About} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
