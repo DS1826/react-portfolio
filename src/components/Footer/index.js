d8594062f4b3ed70599bc8a3c640a32ecc424978
Dina Saunders <dina_saunders@mac.com>
2021-01-10 13:41:11 -0500

    updated footer style properties

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 function Footer() {
     return (
-        <footer class="footer mt-auto py-3">
+        <footer class="footer bg-dark mt-auto py-3">
             <div class="container">
                 <span class="text-muted">DSaunders Portfolio 2021</span>
             </div>
