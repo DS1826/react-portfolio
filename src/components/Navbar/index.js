555d4259d84eaed466b413c84162184b88fae002
Dina Saunders <dina_saunders@mac.com>
2021-01-11 17:34:01 -0500

    added social media links updated page links and style properties

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,25 +1,56 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./style.css";
 
 function Navbar() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav class="navbar navbar-expand-lg navbar-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" >DSaunders</a>
+                <Link className="navbar-brand" to="/" >DSaunders</Link>
                 {/* <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button> */}
                 <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" >Home</a>
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link
+                                to="/about"
+                                className={window.location.pathname === "/about" ? "nav-link active" : "nav-link"}
+                            >
+                                About
+                            </Link>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" >About</a>
+                        <li className="nav-item">
+                        <Link
+                            to="/portfolio"
+                            className={window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
+                        >
+                            Portfolio
+                        </Link>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" >Portfolio</a>
+
+                        <li className="nav-item">
+                            <Link
+                                to="/contact"
+                                className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}
+                            >
+                                Contact
+                        </Link>
                         </li>
+                    </ul>
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item"><a className="li-ic" href="https://www.linkedin.com/in/dina-saunders" target="_blank" rel="noreferrer">
+                            <i class="fab fa-linkedin fa-lg mr-md-5 mr-3"> </i>
+                        </a></li>
+
+                        <li className="nav-item"><a className="git-ic" href="https://github.com/DS1826" target="_blank" rel="noreferrer">
+                            <i class="fab fa-github fa-lg mr-md-5 mr-3"></i></a></li>
+
+                        <li className="nav-item"><a class="email-ic" href="mailto: dina.m.1826@gmail.com">
+                            <i class="fas fa-envelope fa-lg white-text mr-md-5 mr-3 fa-lg"></i></a></li>
+
+                        <li className="nav-item"><a class="phone-ic" href="tel:+14076978530"><i
+                            class="fas fa-phone fa-lg white text mr-md-5 mr-3 fa-lg"></i></a></li>
                     </ul>
                 </div>
             </div>
