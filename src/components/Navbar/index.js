76031b88cf701bcdf4cd556870b944f70ddb32dd
Dina Saunders <dina_saunders@mac.com>
2021-01-12 18:02:57 -0500

    fixed toggler for navbar collapse button

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 
 function Navbar() {
+
+    const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+    const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
     return (
         <nav class="navbar navbar-expand-lg navbar-dark">
             <div class="container-fluid">
                 <Link className="navbar-brand" to="/" >DSaunders</Link>
-                {/* <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={handleNavCollapse}>
       <span class="navbar-toggler-icon"></span>
-    </button> */}
-                <div class="collapse navbar-collapse" id="navbarNav">
+    </button>
+                <div class={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
                             <Link
