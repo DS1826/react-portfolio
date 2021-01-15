651ae5a2f092111d08824cda9a6600379ea6dbed
Dina Saunders <dina_saunders@mac.com>
2021-01-15 15:15:40 -0500

    fixed footer floating over content

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,11 +4,10 @@ import "./style.css";
 function Footer() {
     return (
         <footer class="footer mt-auto text-center">
-            <div class="container py-3">
+            <div class="container py-2">
                 <p>DS Portfolio &copy; 2021</p>
             </div>
         </footer>
-
     );
 }
 
