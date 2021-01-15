fd30c47979e679c7ea47ea7b5fb3c24e8eb95949
Dina Saunders <dina_saunders@mac.com>
2021-01-15 17:36:40 -0500

    added redirect route to home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
@@ -15,7 +15,10 @@ function App() {
       <div className="App">
         <Navbar />
         <Wrapper>
-          <Route exact path="/" component={Home} />
+          <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
+          <Route exact path="/home" component={Home} />
           <Route exact path="/react-portfolio" component={Home} />
           <Route exact path="/about" component={About} />
           <Route exact path="/portfolio" component={Portfolio} />
